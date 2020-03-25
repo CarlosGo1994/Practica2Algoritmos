9dc38d592fcc676f66a9b0f3f1a95c6faaee355f
CarlosGo1994 <charls21ronald@gmail.com>
2020-03-24 18:03:37 -0600

    Datatypes in Java

---
+++ src/com/practica2algoritmos/app/practica2algoritmos.java
@@ -0,0 +1,29 @@
+package com.practica2algoritmos.app;
+
+public class practica2algoritmos {
+
+	public static void main(String[] args) {
+		
+		
+		String nombreEstudiante="Ari";
+		int edadEstudiante=15;
+		int telefonoEstudiante=3157896;
+		boolean isCascada = true;
+		char sexoEstudiante='F';
+		double estaturaEstudiante=1.50;
+		float calificacionEstudiante=3.2f;
+		byte Estudiante=1;
+		short vecesSaleShoping=200;
+		
+		System.out.println("Dame tu nombre:"+nombreEstudiante);
+		System.out.println("Dame tu edad:"+edadEstudiante);
+		System.out.println("Este es un entero:"+telefonoEstudiante);
+		System.out.println("Este es un boleano:"+isCascada);
+		System.out.println("Este es un caracter:"+sexoEstudiante);
+		System.out.println("Este es un double:"+estaturaEstudiante);
+		System.out.println("Este es un float:"+calificacionEstudiante);
+		System.out.println("Este es un byte:"+Estudiante);
+		System.out.println("Este es un short:"+vecesSaleShoping);
+	}
+
+}
